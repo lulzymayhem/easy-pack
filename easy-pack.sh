aad697c6e32d2bf614c803f716beaaf459e731a0
lulzymayhem <jebediahknowsall@gmail.com>
2016-10-12 22:59:57 -0700

    Update easy-pack.sh
    fixed

--- easy-pack.sh
+++ easy-pack.sh
@@ -7,23 +7,18 @@ if [[ $todo == "install" ]]; then
 	echo What would you like to install?
 	read install
 	sudo pacman -S install
-fi
 elif [[ $todo == "remove" ]]; then
 	echo What would you like to remove?
 	read toremove
 	sudo pacman -Rns $toremove
-fi
 elif [[ $todo == "update" ]]; then
 	sudo pacman -Syu
-fi
 elif [[ $todo == "synchronize" ]]; then
 	sudo pacman -Syy
-fi
 elif [[ $todo == "search" ]]; then
 	echo "What do you want to search?"
 	read search
 	sudo pacman -Ss $search
-fi
 else
 	echo "This is not an option."
 fi
