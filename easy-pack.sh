77759fc15c5bd2854c3ce65106a44a8a0ae3aeea
lulzymayhem <jebediahknowsall@gmail.com>
2016-10-12 22:50:02 -0700

    Update easy-pack.sh
    Added more options.

--- easy-pack.sh
+++ easy-pack.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
-echo What distribution of Linux are you using?
-read distro
-if [[ $distro == "arch" ]]; then
-	echo What do you want to do?
-	read todo
-fi
-	if [[ $todo == "install" ]]; then
+
+echo "What do you want to do?"
+read todo
+
+if [[ $todo == "install" ]]; then
 	echo What would you like to install?
 	read install
 	sudo pacman -S install
-	fi
-	elif [[ $todo == "remove" ]]; then
+fi
+elif [[ $todo == "remove" ]]; then
 	echo What would you like to remove?
 	read toremove
 	sudo pacman -Rns $toremove
+fi
+elif [[ $todo == "update" ]]; then
+	sudo pacman -Syu
+fi
+elif [[ $todo == "synchronize" ]]; then
+	sudo pacman -Syy
+fi
+elif [[ $todo == "search" ]]; then
+	echo "What do you want to search?"
+	read search
+	sudo pacman -Ss $search
+fi
+else
+	echo "This is not an option."
 fi
