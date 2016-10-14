912348db9424257fa59987674a645960a9064a1a
lulzymayhem <jebediahknowsall@gmail.com>
2016-10-13 19:44:55 -0700

    Update easypack-install.sh
    Can now make usable on Debian systems.

--- easypack-install.sh
+++ easypack-install.sh
@@ -3,3 +3,7 @@ chmod 755 easy-pack.sh
 current=$( pwd )
 sudo cp $current/easy-pack.sh /bin
 sudo echo alias easy-pack=\"easy-pack.sh\" > /etc/bash.bashrc
+sudo touch /etc/easy-pack.conf
+echo "What is your Linux distro? If using a distro with the apt package manager (Debian, Ubuntu, etc.) please enter \"Debian\" (without quotes) as the distro. If using Arch Linux, please enter \"Arch\" (without the quotes) as the distro.
+read distro
+echo $distro > /etc/easy-pack.conf
