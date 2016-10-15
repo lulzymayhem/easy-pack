baaf94d1cf4e729e45583708e13fe66b4e35bb61
lulzymayhem <jebediahknowsall@gmail.com>
2016-10-15 13:35:15 -0700

    Update easypack-install.sh
    Debugged

--- easypack-install.sh
+++ easypack-install.sh
@@ -4,7 +4,7 @@ current=$( pwd )
 sudo cp $current/easy-pack.sh /bin
 sudo mkdir /etc/easy-pack
 sudo cp /etc/bash.bashrc /etc/easy-pack
-sudo echo alias easy-pack=\"easy-pack.sh\" > /etc/bash.bashrc
+sudo echo alias easy-pack=\"easy-pack.sh\" >> /etc/bash.bashrc
 sudo touch /etc/easy-pack/easy-pack.conf
 echo "What is your Linux distro? If using a distro with the apt package manager (Debian, Ubuntu, etc.) please enter \"Debian\" (without quotes) as the distro. If using Arch Linux, please enter \"Arch\" (without the quotes) as the distro."
 read distro
